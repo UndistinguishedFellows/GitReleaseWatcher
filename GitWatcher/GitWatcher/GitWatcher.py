65197a663749bb96f31c8fa84befd4d5f2e9209b
CapitanLiteral <anaraguen@gmail.com>
2017-12-09 20:36:55 +0100

    release list

--- GitWatcher/GitWatcher/GitWatcher.py
+++ GitWatcher/GitWatcher/GitWatcher.py
@@ -12,6 +12,7 @@ class Window():
 		self.tb_user = QtWidgets.QLineEdit("undistinguishedfellows")
 
 		self.dropDownRepos = QtWidgets.QComboBox()
+		self.dropDownReleases = QtWidgets.QComboBox()
 
 
 		#repo tb
@@ -28,6 +29,7 @@ class Window():
 		self.v_box.addWidget(self.bt_load_repos)
 
 		self.v_box.addWidget(self.dropDownRepos)
+		self.v_box.addWidget(self.dropDownReleases)
 		#self.v_box.addWidget(self.lb_repo)
 		#self.v_box.addWidget(self.tb_repo)
 
@@ -36,19 +38,45 @@ class Window():
 		self.v_box.addLayout(self.v_boxRequest)
 
 		self.w.setLayout(self.v_box)
-
+		
 		self.bt_refresh.clicked.connect(lambda: self.refresh_click(self.tb_user.text(), self.dropDownRepos.currentText()))
 		self.bt_load_repos.clicked.connect(lambda: self.loadRepos(self.tb_user.text()))
+		self.dropDownRepos.currentIndexChanged.connect(self.selection_change)
 		self.w.setWindowTitle("GitWatcher")
 
-
-	def refresh_click(self, user, repo):
-		url = "https://api.github.com/repos/" + str(user) + "/" + str(repo) + "/releases/latest"
+	def selection_change(self, i):
+		url = "https://api.github.com/repos/" + str(self.tb_user.text()) + "/" + str(self.dropDownRepos.currentText()) + "/releases" #https://api.github.com/users/CapitanLiteral/repos
 		print(url)
 		r = requests.get(url)
+		self.releaseList = []
+		if (r.ok):
+			self.jReleases = json.loads(r.text or r.content)
+			i = 0
+			for item in self.jReleases:
+				pair = []
+				pair.append(item["tag_name"])
+				pair.append(item["id"])
+				self.releaseList.append(pair)
+			for x in self.releaseList:
+			    self.dropDownReleases.addItem(x[0])
+			
+
+	def refresh_click(self, user, repo):
+		id = 0
+		for x in self.releaseList:
+		    if x[0] == self.dropDownReleases.currentText():
+		        id = x[1]
+		url = "https://api.github.com/repos/" + str(user) + "/" + str(repo) + "/releases/" + str(id)
+		print(url)
+		r = requests.get(url)
+		for i in reversed(range(self.v_boxRequest.count())): 
+			self.v_boxRequest.itemAt(i).widget().setParent(None)
 		if (r.ok):
 			self.repoItem = json.loads(r.text or r.content)
 			self.update()
+		else:
+			lb_name = QtWidgets.QLabel("No releases found or some error ocurred with request.")
+			self.v_boxRequest.addWidget(lb_name)
 			#print(repoItem)
 	def loadRepos(self, user):
 		url = "https://api.github.com/users/" + str(user) + "/repos" #https://api.github.com/users/CapitanLiteral/repos
@@ -65,19 +93,20 @@ class Window():
 
 			#print(repoItem)
 	def update(self):
-		for i in reversed(range(self.v_boxRequest.count())): 
-			self.v_boxRequest.itemAt(i).widget().setParent(None)
 		count = 0
-		for item in self.repoItem['assets']:
-			lb_name = QtWidgets.QLabel(item["name"])
-			lb_downloadCount = QtWidgets.QLabel(str(item["download_count"]))
-			count += item["download_count"]
-			self.v_boxRequest.addWidget(lb_name)
-			self.v_boxRequest.addWidget(lb_downloadCount)
+		if len(self.repoItem['assets']) > 0:
+			for item in self.repoItem['assets']:
+				lb_name = QtWidgets.QLabel(item["name"])
+				lb_downloadCount = QtWidgets.QLabel(str(item["download_count"]))
+				count += item["download_count"]
+				self.v_boxRequest.addWidget(lb_name)
+				self.v_boxRequest.addWidget(lb_downloadCount)
 
-		lb_count = QtWidgets.QLabel(str("Download total count: " + str(count)))
-		self.v_boxRequest.addWidget(lb_count)
-		
+			lb_count = QtWidgets.QLabel(str("Download total count: " + str(count)))
+			self.v_boxRequest.addWidget(lb_count)
+		else:
+			lb_name = QtWidgets.QLabel("No releases found or some error ocurred with request.")
+			self.v_boxRequest.addWidget(lb_name)
 
 
 	def window(self):		
